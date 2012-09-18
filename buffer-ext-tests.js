3c0cf9e86e4c2e3a9aa1a99b4f524c2d6530c1ad
David Wragg <david@wragg.org>
2012-09-18 21:55:22 +0100

    Support for reading signed ints

--- buffer-ext-tests.js
+++ buffer-ext-tests.js
@@ -7,81 +7,184 @@ function h2b(str) {
     return new Buffer(str, "hex");
 }
 
-function test_readUInt(assert, hex, be, le) {
-    var len = hex.length / 2;
-    le = le || be;
-
-    // Straightforward conversion case
-    assert.equal(h2b(hex).readUIntBE(len, 0), be);
-    assert.equal(h2b(hex).readUIntLE(len, 0), le);
-
-    // Reads off the end of the buffer should throw
-    assert.throws(function () { h2b(hex).readUIntBE(len, 1); })
-    assert.throws(function () { h2b(hex).readUIntLE(len, 1); })
-
-    // Node's Buffer doesn't catch negative offsets
-    //assert.throws(function () { h2b(hex).readUIntBE(len, -1); })
-    //assert.throws(function () { h2b(hex).readUIntLE(len, -1); })
-
-    // Test noAssert reads that stray off the ends of the buffer
-    assert.equal(h2b(hex).readUIntBE(len, -1, true),
-                 h2b("00"+hex).readUIntBE(len, 0));
-    assert.equal(h2b(hex).readUIntLE(len, 1, true),
-                 h2b(hex+"00").readUIntLE(len, 1));
-
-    var expect = null;
-    try {
-        // The resulting integer may be too big, in which case
-        // skip this test
-        expect = h2b(hex+"00").readUIntBE(len, 1);
-    } catch (e) {};
-    if (expect != null) {
-        assert.equal(h2b(hex).readUIntBE(len, 1, true), expect);
+// Reverse a buffer in-place
+function reverse(buf) {
+    for (var i = 0, j = buf.length - 1; i < j; i++, j--) {
+        var t = buf[i];
+        buf[i] = buf[j];
+        buf[j] = t;
     }
+    return buf;
+}
 
-    expect = null;
+// Discard exceptions due to integers being too large
+function tooLargeIsOk(f) {
     try {
-        // The resulting integer may be too big, in which case
-        // skip this test
-        expect = h2b("00"+hex).readUIntLE(len, 0);
-    } catch (e) {};
-    if (expect != null) {
-        assert.equal(h2b(hex).readUIntLE(len, -1, true), expect);
+        f();
+    }
+    catch (e) {
+        if (!/attempt to read integer too large to be safely held in a JS number:/.test(e.message)) {
+            throw e;
+        }
     }
 }
 
+function readUInt_case(assert, hex, be) {
+    var len = hex.length / 2;
+
+    // Straightforward conversion case
+    assert.equal(h2b(hex).readUIntBE(len, 0), be);
+    assert.equal(reverse(h2b(hex)).readUIntLE(len, 0), be);
+
+    // Reads off the end of the buffer should throw
+    assert.throws(function () { h2b(hex).readUIntBE(len, 1); })
+    assert.throws(function () { reverse(h2b(hex)).readUIntLE(len, 1); })
+
+    // Node doesn't catch negative offsets
+    //assert.throws(function () { h2b(hex).readUIntBE(len, -1); })
+    //assert.throws(function () { reverse(h2b(hex)).readUIntLE(len, -1); })
+
+    // Test noAssert reads that stray off the ends of the
+    // buffer. These tests can fail due to integers being to
+    // big, which is fine.
+
+    tooLargeIsOk(function () {
+        assert.equal(h2b(hex).readUIntBE(len, -1, true),
+                     h2b("00"+hex).readUIntBE(len, 0));
+    });
+    tooLargeIsOk(function () {
+        assert.equals(reverse(h2b(hex)).readUIntLE(len, 1, true),
+                      reverse(h2b("00"+hex)).readUIntLE(len, 1));
+    });
+    tooLargeIsOk(function () {
+        assert.equal(h2b(hex).readUIntBE(len, 1, true),
+                     h2b(hex+"00").readUIntBE(len, 1));
+    });
+    tooLargeIsOk(function () {
+        assert.equal(reverse(h2b(hex)).readUIntLE(len, -1, true),
+                     reverse(h2b(hex+"00")).readUIntLE(len, 0))
+    });
+}
+
 module.exports.readUInt = function (assert) {
-    test_readUInt(assert, "00", 0x00);
-    test_readUInt(assert, "01", 0x01);
-    test_readUInt(assert, "ff", 0xff);
+    readUInt_case(assert, "00", 0x00);
+    readUInt_case(assert, "01", 0x01);
+    readUInt_case(assert, "ff", 0xff);
 
-    test_readUInt(assert, "0000", 0x0000);
-    test_readUInt(assert, "0102", 0x0102, 0x0201);
-    test_readUInt(assert, "ffff", 0xffff);
+    readUInt_case(assert, "0000", 0x0000);
+    readUInt_case(assert, "0102", 0x0102);
+    readUInt_case(assert, "ffff", 0xffff);
 
-    test_readUInt(assert, "000000", 0x000000);
-    test_readUInt(assert, "010203", 0x010203, 0x030201);
-    test_readUInt(assert, "ffffff", 0xffffff);
+    readUInt_case(assert, "000000", 0x000000);
+    readUInt_case(assert, "010203", 0x010203);
+    readUInt_case(assert, "ffffff", 0xffffff);
 
-    test_readUInt(assert, "00000000", 0x00000000);
-    test_readUInt(assert, "01020304", 0x01020304, 0x04030201);
-    test_readUInt(assert, "ffffffff", 0xffffffff);
+    readUInt_case(assert, "00000000", 0x00000000);
+    readUInt_case(assert, "01020304", 0x01020304);
+    readUInt_case(assert, "ffffffff", 0xffffffff);
 
-    test_readUInt(assert, "0000000000", 0x0000000000);
-    test_readUInt(assert, "0102030405", 0x0102030405, 0x0504030201);
-    test_readUInt(assert, "ffffffffff", 0xffffffffff);
+    readUInt_case(assert, "0000000000", 0x0000000000);
+    readUInt_case(assert, "0102030405", 0x0102030405);
+    readUInt_case(assert, "ffffffffff", 0xffffffffff);
 
-    test_readUInt(assert, "000000000000", 0x000000000000);
-    test_readUInt(assert, "010203040506", 0x010203040506, 0x060504030201);
-    test_readUInt(assert, "ffffffffffff", 0xffffffffffff);
+    readUInt_case(assert, "000000000000", 0x000000000000);
+    readUInt_case(assert, "010203040506", 0x010203040506);
+    readUInt_case(assert, "ffffffffffff", 0xffffffffffff);
 
-    test_readUInt(assert, "00000000000000", 0x00000000000000);
-    test_readUInt(assert, "01020304050607", 0x01020304050607, 0x07060504030201);
-    test_readUInt(assert, "1fffffffffff1f", 0x1fffffffffff1f);
+    readUInt_case(assert, "00000000000000", 0x00000000000000);
+    readUInt_case(assert, "01020304050607", 0x01020304050607);
+    readUInt_case(assert, "1fffffffffffff", 0x1fffffffffffff);
 
-    test_readUInt(assert, "0000000000000000", 0x0000000000000000);
-    test_readUInt(assert, "0001020304050600", 0x0001020304050600, 0x0006050403020100);
-    test_readUInt(assert, "001fffffffff1f00", 0x001fffffffff1f00);
+    readUInt_case(assert, "0000000000000000", 0x0000000000000000);
+    readUInt_case(assert, "0002030405060708", 0x0002030405060708);
+    readUInt_case(assert, "001fffffffffffff", 0x001fffffffffffff);
+
+    assert.done();
+};
+
+function readInt_case(assert, hex, be) {
+    var len = hex.length / 2;
+
+    // Straightforward conversion case
+    assert.equal(h2b(hex).readIntBE(len, 0), be);
+    assert.equal(reverse(h2b(hex)).readIntLE(len, 0), be);
+
+    // Reads off the end of the buffer should throw
+    assert.throws(function () { h2b(hex).readIntBE(len, 1); })
+    assert.throws(function () { reverse(h2b(hex)).readIntLE(len, 1); })
+
+    // Node doesn't catch negative offsets
+    //assert.throws(function () { h2b(hex).readIntBE(len, -1); })
+    //assert.throws(function () { reverse(h2b(hex)).readIntLE(len, -1); })
+
+    // Test noAssert reads that stray off the ends of the
+    // buffer. These tests can fail due to integers being to
+    // big, which is fine.
+
+    tooLargeIsOk(function () {
+        assert.equal(h2b(hex).readIntBE(len, -1, true),
+                     h2b("00"+hex).readIntBE(len, 0));
+    });
+    tooLargeIsOk(function () {
+        assert.equals(reverse(h2b(hex)).readIntLE(len, 1, true),
+                      reverse(h2b("00"+hex)).readIntLE(len, 1));
+    });
+    tooLargeIsOk(function () {
+        assert.equal(h2b(hex).readIntBE(len, 1, true),
+                     h2b(hex+"00").readIntBE(len, 1));
+    });
+    tooLargeIsOk(function () {
+        assert.equal(reverse(h2b(hex)).readIntLE(len, -1, true),
+                     reverse(h2b(hex+"00")).readIntLE(len, 0))
+    });
+}
+
+module.exports.readInt = function (assert) {
+    readInt_case(assert, "00", 0x00);
+    readInt_case(assert, "01", 0x01);
+    readInt_case(assert, "7f", 0x7f);
+    readInt_case(assert, "80", -0x80);
+    readInt_case(assert, "ff", -0x01);
+
+    readInt_case(assert, "0000", 0x0000);
+    readInt_case(assert, "0102", 0x0102);
+    readInt_case(assert, "7fff", 0x7fff);
+    readInt_case(assert, "8000", -0x8000);
+    readInt_case(assert, "ffff", -0x0001);
+
+    readInt_case(assert, "000000", 0x000000);
+    readInt_case(assert, "010203", 0x010203);
+    readInt_case(assert, "7fffff", 0x7fffff);
+    readInt_case(assert, "800000", -0x800000);
+    readInt_case(assert, "ffffff", -0x000001);
+
+    readInt_case(assert, "00000000", 0x00000000);
+    readInt_case(assert, "01020304", 0x01020304);
+    readInt_case(assert, "7fffffff", 0x7fffffff);
+    readInt_case(assert, "80000000", -0x80000000);
+    readInt_case(assert, "ffffffff", -0x00000001);
+
+    readInt_case(assert, "0000000000", 0x0000000000);
+    readInt_case(assert, "0102030405", 0x0102030405);
+    readInt_case(assert, "7fffffffff", 0x7fffffffff);
+    readInt_case(assert, "8000000000", -0x8000000000);
+    readInt_case(assert, "ffffffffff", -0x0000000001);
+
+    readInt_case(assert, "000000000000", 0x000000000000);
+    readInt_case(assert, "010203040506", 0x010203040506);
+    readInt_case(assert, "7fffffffffff", 0x7fffffffffff);
+    readInt_case(assert, "800000000000", -0x800000000000);
+    readInt_case(assert, "ffffffffffff", -0x000000000001);
+
+    readInt_case(assert, "00000000000000", 0x00000000000000);
+    readInt_case(assert, "01020304050607", 0x01020304050607);
+    readInt_case(assert, "1fffffffffffff", 0x1fffffffffffff);
+    readInt_case(assert, "e0000000000001", -0x1fffffffffffff);
+
+    readInt_case(assert, "0000000000000000", 0x0000000000000000);
+    readInt_case(assert, "0002030405060708", 0x0002030405060708);
+    readInt_case(assert, "001fffffffffffff", 0x001fffffffffffff);
+    readInt_case(assert, "ffe0000000000001", -0x001fffffffffffff);
 
     assert.done();
 };
--- buffer-ext.js
+++ buffer-ext.js
@@ -7,8 +7,8 @@ var SHIFT_LEFT_32 = (1 << 16) * (1 << 16);
 var MAX_INT = 9007199254740991;
 
 // Buffer.prototype.readUInt8 returns undefined if the offset is
-// outside of the buffer, unlike the other widths.  This provides
-// consistency.
+// outside of the buffer, unlike for other widths.  This function
+// makes it consistent.
 function consistent_readUInt8 (offset, noAssert) {
     return this.readUInt8(offset, noAssert) || 0;
 }
@@ -109,6 +109,116 @@ var UIntLE_readers = [
 
 Buffer.prototype.readUIntLE = function (len, offset, noAssert) {
     var reader = UIntLE_readers[len];
+    if (reader) {
+        return reader.call(this, offset, noAssert);
+    } else {
+        throw new Error("Cannot read integer of length " + len);
+    }
+};
+
+
+// Buffer.prototype.readInt8 returns undefined if the offset is
+// outside of the buffer, unlike for other widths.  This function
+// makes it consistent.
+function consistent_readInt8 (offset, noAssert) {
+    return this.readInt8(offset, noAssert) || 0;
+}
+
+Buffer.prototype.readInt24BE = function (offset, noAssert) {
+    return (this.readInt8(offset, noAssert) << 16) + this.readUInt16BE(offset + 1, noAssert);
+};
+
+Buffer.prototype.readInt40BE = function (offset, noAssert) {
+    return (this.readInt8(offset, noAssert) || 0) * SHIFT_LEFT_32 + this.readUInt32BE(offset + 1, noAssert);
+};
+
+Buffer.prototype.readInt48BE = function (offset, noAssert) {
+    return this.readInt16BE(offset, noAssert) * SHIFT_LEFT_32 + this.readUInt32BE(offset + 2, noAssert);
+};
+
+Buffer.prototype.readInt56BE = function (offset, noAssert) {
+    var res = (((this.readInt8(offset, noAssert) || 0) << 16) + this.readUInt16BE(offset + 1, noAssert)) * SHIFT_LEFT_32 + this.readUInt32BE(offset + 3, noAssert);
+    if (res <= MAX_INT && res >= -MAX_INT) {
+        return res;
+    } else {
+        throw new Error("attempt to read integer too large to be safely held in a JS number: " + res);
+    }
+};
+
+Buffer.prototype.readInt64BE = function (offset, noAssert) {
+    var res = this.readInt32BE(offset, noAssert) * SHIFT_LEFT_32 + this.readUInt32BE(offset + 4, noAssert);
+    if (res <= MAX_INT && res >= -MAX_INT) {
+        return res;
+    } else {
+        throw new Error("attempt to read integer too large to be safely held in a JS number: " + res);
+    }
+};
+
+var IntBE_readers = [
+    false,
+    consistent_readInt8,
+    Buffer.prototype.readInt16BE,
+    Buffer.prototype.readInt24BE,
+    Buffer.prototype.readInt32BE,
+    Buffer.prototype.readInt40BE,
+    Buffer.prototype.readInt48BE,
+    Buffer.prototype.readInt56BE,
+    Buffer.prototype.readInt64BE
+];
+
+Buffer.prototype.readIntBE = function (len, offset, noAssert) {
+    var reader = IntBE_readers[len];
+    if (reader) {
+        return reader.call(this, offset, noAssert);
+    } else {
+        throw new Error("Cannot read integer of length " + len);
+    }
+};
+
+Buffer.prototype.readInt24LE = function (offset, noAssert) {
+    return (this.readInt8(offset + 2, noAssert) << 16) + this.readUInt16LE(offset, noAssert);
+};
+
+Buffer.prototype.readInt40LE = function (offset, noAssert) {
+    return (this.readInt8(offset + 4, noAssert) || 0) * SHIFT_LEFT_32 + this.readUInt32LE(offset, noAssert);
+};
+
+Buffer.prototype.readInt48LE = function (offset, noAssert) {
+    return this.readInt16LE(offset + 4, noAssert) * SHIFT_LEFT_32 + this.readUInt32LE(offset, noAssert);
+};
+
+Buffer.prototype.readInt56LE = function (offset, noAssert) {
+    var res = (((this.readInt8(offset + 6, noAssert) || 0) << 16) + this.readUInt16LE(offset + 4, noAssert)) * SHIFT_LEFT_32 + this.readUInt32LE(offset, noAssert);
+    if (res <= MAX_INT && res >= -MAX_INT) {
+        return res;
+    } else {
+        throw new Error("attempt to read integer too large to be safely held in a JS number: " + res);
+    }
+};
+
+Buffer.prototype.readInt64LE = function (offset, noAssert) {
+    var res = this.readInt32LE(offset + 4, noAssert) * SHIFT_LEFT_32 + this.readUInt32LE(offset, noAssert);
+    if (res <= MAX_INT && res >= -MAX_INT) {
+        return res;
+    } else {
+        throw new Error("attempt to read integer too large to be safely held in a JS number: " + res);
+    }
+};
+
+var IntLE_readers = [
+    false,
+    consistent_readInt8,
+    Buffer.prototype.readInt16LE,
+    Buffer.prototype.readInt24LE,
+    Buffer.prototype.readInt32LE,
+    Buffer.prototype.readInt40LE,
+    Buffer.prototype.readInt48LE,
+    Buffer.prototype.readInt56LE,
+    Buffer.prototype.readInt64LE
+];
+
+Buffer.prototype.readIntLE = function (len, offset, noAssert) {
+    var reader = IntLE_readers[len];
     if (reader) {
         return reader.call(this, offset, noAssert);
     } else {
